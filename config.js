1e5e54ae442d6aff0a5faa0207d0fa037121a553
dossbaby <dossbb@naver.com>
2025-05-28 10:41:28 +0900

    yes

---
+++ config.js
@@ -0,0 +1,10 @@
+// ✅ Firebase 설정 추가
+const firebaseConfig = {
+  apiKey: "AIzaSyCipUm4FyEJ5qb2jJ7HEsquLPT9ZMJsGVo",
+  authDomain: "todo-app-doss.firebaseapp.com",
+  projectId: "todo-app-doss",
+  storageBucket: "todo-app-doss.firebasestorage.app",
+  messagingSenderId: "477583861361",
+  appId: "1:477583861361:web:1a15bbed10c6b846eaddd0",
+  measurementId: "G-9344VTECSY",
+};
