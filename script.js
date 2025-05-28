68a3cfea62f1eab6288829d07b8f318277940780
dossbaby <dossbb@naver.com>
2025-05-28 10:31:12 +0900

    update

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// ✅ Firebase 설정 추가
-const firebaseConfig = {
-  apiKey: "AIzaSyCipUm4FyEJ5qb2jJ7HEsquLPT9ZMJsGVo",
-  authDomain: "todo-app-doss.firebaseapp.com",
-  projectId: "todo-app-doss",
-  storageBucket: "todo-app-doss.firebasestorage.app",
-  messagingSenderId: "477583861361",
-  appId: "1:477583861361:web:1a15bbed10c6b846eaddd0",
-  measurementId: "G-9344VTECSY",
-};
-
 // Firebase 초기화
 const app = firebase.initializeApp(firebaseConfig);
 // Firestore 참조 얻기
